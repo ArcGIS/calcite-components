67f63e237b798422f5a87ef2875deb2c82a1e8ee
Branden Chong <158607603+brandentheintern@users.noreply.github.com>
2024-04-12 13:18:29 -0700

    Benelan/storybook custom elements build (#9125)
    **Related Issue:** #
    
    ## Summary
    
    ---------
    
    Co-authored-by: Ben Elan <no-reply@benelan.dev>

--- packages/calcite-components/stencil.storybook.config.ts
+++ packages/calcite-components/stencil.storybook.config.ts
@@ -1,14 +1,21 @@
-import { OutputTargetWww } from "@stencil/core/internal";
+import { OutputTargetDist, OutputTargetDistCustomElements } from "@stencil/core/internal";
 import { create as baseConfigCreator } from "./stencil.config";
 
 export const create: typeof baseConfigCreator = () => {
   const docsConfig = baseConfigCreator();
 
-  const wwwOutputTarget = docsConfig.outputTargets?.find((element) => element.type === "www") as OutputTargetWww;
-  wwwOutputTarget.copy = wwwOutputTarget.copy?.filter((item) => !item.src.includes("demos"));
-  wwwOutputTarget.dir = "__docs-temp__";
-  docsConfig.outputTargets = [wwwOutputTarget];
+  const componentsOutputTarget = docsConfig.outputTargets?.find(
+    (element) => element.type === "dist-custom-elements",
+  ) as OutputTargetDistCustomElements;
+  componentsOutputTarget.dir = "__docs-temp__/components";
 
+  const distOutputTarget = docsConfig.outputTargets?.find((element) => element.type === "dist") as OutputTargetDist;
+  distOutputTarget.buildDir = "../__docs-temp__";
+  // distOutputTarget.collectionDir = "../__docs-temp__/collection";
+  // distOutputTarget.typesDir = "../__docs-temp__/types";
+  // distOutputTarget.esmLoaderPath = "../__docs-temp__/loader";
+
+  docsConfig.outputTargets = [componentsOutputTarget, distOutputTarget];
   return docsConfig;
 };
 
